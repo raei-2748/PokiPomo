24f55456c3c414bc26ca935d1c3bb6ae754e61af
Ray <167051212+ieunfnjqmmm@users.noreply.github.com>
2026-01-26 12:55:48 +1100

    Initial Commit

---
+++ PokiPomo App/PokiPomo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokiPomo_AppApp.swift
+//  PokiPomo App
+//
+//  Created by Ray Wang on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct PokiPomo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokiPomo AppTests/PokiPomo_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokiPomo_AppTests.swift
+//  PokiPomo AppTests
+//
+//  Created by Ray Wang on 1/26/26.
+//
+
+import Testing
+@testable import PokiPomo_App
+
+struct PokiPomo_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
